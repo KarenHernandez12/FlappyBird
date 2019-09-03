d35504efa4a6daea2a4869c955da0b984ce9c55c
KarenHernandez12 <54598364+KarenHernandez12@users.noreply.github.com>
2019-09-02 21:43:45 -0500

    Add files via upload

---
+++ Assets/Scripts/DelayFlapToRestart.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayFlapToRestart : MonoBehaviour
+{
+    public GameObject flapToRestart;
+    public float delay= 1f;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        Invoke("EnableFlapToRestart",delay);
+    }
+
+    void EnableFlapToRestart(){
+        flapToRestart.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SoundSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSystem : MonoBehaviour
+{
+    public static SoundSystem instance;
+
+    private void Awake(){
+        if (SoundSystem.instance==null){
+            SoundSystem.instance=this;
+        }else if (SoundSystem.instance!=this){
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnDestroy(){
+        if(SoundSystem.instance==this){
+            SoundSystem.instance=null;
+        }
+    }
+}
